4658bafa4305a914df59a673e15f9703202baac5
Dr-Paganini <31491523+Dr-Paganini@users.noreply.github.com>
2017-09-02 01:02:39 -0600

    Add files via upload

---
+++ RPi_GPIO_blink.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import tkinter
+import random
+import time
+import RPi.GPIO as GPIO
+#tells the pi to set pin 7 as an output
+GPIO.setup(7, GPIO.OUT)
+
+GPIO.setmode(GPIO.BOARD)
+
+for x in range(0, 3):
+    GPIO.output(7, True) # Turns the 7th switch on
+    time.sleep(1)
+    GPIO.output(7, False) # Turns the 7th switch off
+    time.sleep(1)
+GPIO.cleanup() #Sets the GPIO back to nuetral
+
